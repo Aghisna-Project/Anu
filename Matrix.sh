3fe9507bf97e1009b5ed8ce46259e4cc46f9142a
Amicia De Rune <rooghz720@gmail.com>
2025-03-09 15:58:20 +0700

    Update Matrix.sh
    Fsf

--- Matrix.sh
+++ Matrix.sh
@@ -7,7 +7,6 @@ declare -A repos=(
     ["https://github.com/Aghisna-Project/vendor_xiaomi_sm6150-common -b 15"]="vendor/xiaomi/sm6150-common"
     ["https://github.com/Aghisna-Project/vendor_xiaomi_sweet -b 15"]="vendor/xiaomi/sweet"
     ["https://github.com/Aghisna-Project/android_hardware_xiaomi -b 15"]="hardware/xiaomi"
-    ["https://github.com/Aghisna-Project/android_xiaomi_kernel_sweet -b build"]="kernel/xiaomi/sm6150"
     ["https://repo.radio/donjohanliebert/vendor_xiaomi_sweet-miuicamera -b udc"]="vendor/xiaomi/sweet-miuicamera"
 )
 
@@ -24,7 +23,10 @@ for repo in "${!repos[@]}"; do
     # Masuk ke direktori tujuan
     cd "$target_dir"
 
-    # Melakukan git clone
-    git clone $repo
+    # Melakukan git clone dengan nama direktori target
+    git clone $repo .
     echo "Repository $repo telah berhasil di-clone ke $target_dir."
+    
+    # Kembali ke direktori sebelumnya
+    cd -
 done
