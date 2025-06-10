b43e584fd5aaab2dc21fbdef42c27fb39f910be9
Amicia De Rune <rooghz720@gmail.com>
2025-06-10 14:43:58 +0700

    update patch infinityPatch2.9.sh

--- infinityPatch2.9.sh
+++ infinityPatch2.9.sh
@@ -5,21 +5,28 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Direktori utama: $SCRIPT_DIR"
 
-PATCH_URL="https://raw.githubusercontent.com/Aghisna-Project/Anu/main/0001-add-back-etar-and-twelve.patch"
-PATCH_FILE="0001-add-back-etar-and-twelve.patch"
-PATCH_DEST="$SCRIPT_DIR/vendor/infinity/config/$PATCH_FILE"
+# PATCH_URL="https://raw.githubusercontent.com/Aghisna-Project/Anu/main/0001-add-back-etar-and-twelve.patch"
+# PATCH_FILE="0001-add-back-etar-and-twelve.patch"
+# PATCH_DEST="$SCRIPT_DIR/vendor/extras/config/$PATCH_FILE"
 
 # Pindah ke direktori utama
 cd "$SCRIPT_DIR"
 
-echo "Cloning aplikasi Musik dan Kalender..."
+echo "clean direktori"
+rm -rf "$SCRIPT_DIR/packages/apps/Calendar"
+rm -rf "$SCRIPT_DIR/vendor/extras"
+rm -rf "$SCRIPT_DIR/hardware/xiaomi"
+
+sleep 5
+
+echo "Cloning aplikasi"
 git clone https://github.com/LineageOS/android_packages_apps_Twelve.git -b lineage-22.2 "$SCRIPT_DIR/packages/apps/Twelve"
 git clone https://github.com/LineageOS/android_packages_apps_Etar.git -b lineage-22.2 "$SCRIPT_DIR/packages/apps/Etar"
-rm -rf "$SCRIPT_DIR/packages/apps/Calendar"
+git clone https://github.com/Aghisna-Project/vendor_extras.git -b 15 "$SCRIPT_DIR/vendor/extras"
 
-echo "Mengunduh patch..."
-curl -L -o "$PATCH_FILE" "$PATCH_URL"
-mv "$PATCH_FILE" "$PATCH_DEST"
+# echo "Mengunduh patch..."
+# curl -L -o "$PATCH_FILE" "$PATCH_URL"
+# mv "$PATCH_FILE" "$PATCH_DEST"
 
 echo "Clone dan download patch selesai. Revert ikon dimulai..."
 
@@ -38,7 +45,6 @@ cd "$SCRIPT_DIR/vendor/infinity"
 git am "config/$PATCH_FILE"
 
 echo "clone device tree"
-rm -rf "$SCRIPT_DIR/hardware/xiaomi"
 sleep 10
 git clone https://github.com/Aghisna-Project/android_hardware_xiaomi.git -b 15-new "$SCRIPT_DIR/hardware/xiaomi"
 git clone https://github.com/Aghisna-Project/Vantom_barnul_sm6150.git "$SCRIPT_DIR/kernel/xiaomi/sm6150"
