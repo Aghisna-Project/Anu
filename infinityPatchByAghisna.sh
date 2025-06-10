8f937de3efb68033a31c7170b5a1c95ffc66a2e7
Amicia De Rune <rooghz720@gmail.com>
2025-06-10 15:05:09 +0700

    infinityPatchByAghisna

--- infinityPatchByAghisna.sh
+++ infinityPatchByAghisna.sh
@@ -39,4 +39,23 @@ git reset --hard HEAD~1
 cd "$SCRIPT_DIR/packages/apps/Contacts"
 git reset --hard HEAD~1
 
+echo "clean direktori befor clone dt"
+rm -rf "$SCRIPT_DIR/hardware/xiaomi"
+rm -rf "$SCRIPT_DIR/kernel/xiaomi/sm6150"
+rm -rf "$SCRIPT_DIR/vendor/xiaomi/sweet-miuicamera"
+rm -rf "$SCRIPT_DIR/device/xiaomi/sm6150-common"
+rm -rf "$SCRIPT_DIR/device/xiaomi/sweet"
+rm -rf "$SCRIPT_DIR/vendor/xiaomi/sm6150-common"
+rm -rf "$SCRIPT_DIR/vendor/xiaomi/sweet"
+
+echo "clone device tree"
+sleep 10
+git clone --depth=1 https://github.com/Aghisna-Project/android_hardware_xiaomi.git -b 15-new "$SCRIPT_DIR/hardware/xiaomi"
+git clone --depth=1 https://github.com/Aghisna-Project/Vantom_barnul_sm6150.git "$SCRIPT_DIR/kernel/xiaomi/sm6150"
+git clone --depth=1 https://gitea.com/RooGhz720/leica-sweet.git "$SCRIPT_DIR/vendor/xiaomi/sweet-miuicamera"
+git clone --depth=1 https://gitea.com/RooGhz720/device_xiaomi_sm6150-common.git -b 15 "$SCRIPT_DIR/device/xiaomi/sm6150-common"
+git clone --depth=1 https://gitea.com/RooGhz720/device_xiaomi_sweet.git -b 15-new "$SCRIPT_DIR/device/xiaomi/sweet"
+git clone --depth=1 https://gitea.com/RooGhz720/vendor_xiaomi_sm6150-common.git "$SCRIPT_DIR/vendor/xiaomi/sm6150-common"
+git clone --depth=1 https://gitea.com/RooGhz720/vendor_xiaomi_sweet.git "$SCRIPT_DIR/vendor/xiaomi/sweet"
+
 echo "Semua proses selesai."
