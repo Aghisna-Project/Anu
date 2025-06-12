c0e6ad651ddd2714af9969f1dc6a71357833fee0
RooGhz720 <RooGhz720@gmail.com>
2025-06-12 08:00:01 +0800

    fshfiuhf
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- infinityPatchByAghisna.sh
+++ infinityPatchByAghisna.sh
@@ -5,9 +5,9 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Direktori utama: $SCRIPT_DIR"
 
-# PATCH_URL="https://raw.githubusercontent.com/Aghisna-Project/Anu/main/0001-add-back-etar-and-twelve.patch"
-# PATCH_FILE="0001-add-back-etar-and-twelve.patch"
-# PATCH_DEST="$SCRIPT_DIR/vendor/extras/config/$PATCH_FILE"
+PATCH_URL="https://raw.githubusercontent.com/Aghisna-Project/Anu/main/0001-version-separate.patch"
+PATCH_FILE="0001-version-separate.patch"
+PATCH_DEST="$SCRIPT_DIR/vendor/infinity/config/$PATCH_FILE"
 
 # Pindah ke direktori utama
 cd "$SCRIPT_DIR"
@@ -21,12 +21,13 @@ sleep 5
 
 echo "Cloning aplikasi"
 git clone https://github.com/LineageOS/android_packages_apps_Twelve.git -b lineage-22.1 "$SCRIPT_DIR/packages/apps/Twelve"
-git clone https://github.com/LineageOS/android_packages_apps_Etar.git -b lineage-22.1 "$SCRIPT_DIR/packages/apps/Etar"
 git clone https://github.com/Aghisna-Project/vendor_extras.git -b 15 "$SCRIPT_DIR/vendor/extras"
 
-# echo "Mengunduh patch..."
-# curl -L -o "$PATCH_FILE" "$PATCH_URL"
-# mv "$PATCH_FILE" "$PATCH_DEST"
+echo "Mengunduh patch dan terapkan"
+curl -L -o "$PATCH_FILE" "$PATCH_URL"
+mv "$PATCH_FILE" "$PATCH_DEST"
+cd "$SCRIPT_DIR/vendor/infinity/config"
+git am "PATCH_FILE"
 
 echo "Clone dan download patch selesai. Revert ikon dimulai..."
 
@@ -49,7 +50,7 @@ rm -rf "$SCRIPT_DIR/vendor/xiaomi/sm6150-common"
 rm -rf "$SCRIPT_DIR/vendor/xiaomi/sweet"
 
 echo "clone device tree"
-sleep 10
+sleep 5
 git clone --depth=1 https://github.com/Aghisna-Project/android_hardware_xiaomi.git -b 15-new "$SCRIPT_DIR/hardware/xiaomi"
 git clone --depth=1 https://github.com/Aghisna-Project/Vantom_barnul_sm6150.git "$SCRIPT_DIR/kernel/xiaomi/sm6150"
 git clone --depth=1 https://gitea.com/RooGhz720/leica-sweet.git "$SCRIPT_DIR/vendor/xiaomi/sweet-miuicamera"
